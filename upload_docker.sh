0f2f7fd0376f26b2e450dc5201f336e09ad7ef9d
NursultanBeken <nur.beken.usa@gmail.com>
2021-02-09 20:17:26 -0500

    feat: add shell scipts to manage docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,13 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
+dockerpath=nbekenov/app
+ 
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag app:latest $dockerpath
 
-# Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
